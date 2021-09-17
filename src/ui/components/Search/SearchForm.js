cd800ea654a7183b345795bc5f09a04f7ecbdb5c
Evan Winter <evan.winter@brookfieldpropertiesretail.com>
2021-09-16 19:23:46 -0500

    Add note about API issues, and direct users to sample data.

--- src/ui/components/Search/SearchForm.js
+++ src/ui/components/Search/SearchForm.js
@@ -7,62 +7,66 @@ import { useDebounce } from "hooks"
 import ListSuggestions from "./ListSuggestions"
 
 const SearchForm = () => {
-	// State
-	const [query, setQuery] = useState("")
-	const { showSuggestions, results } = useSelector((state) => state.search)
+  // State
+  const [query, setQuery] = useState("")
+  const { showSuggestions, results } = useSelector((state) => state.search)
 
-	// Actions
-	const dispatch = useDispatch()
-	const { appActions, searchActions } = useActions()
+  // Actions
+  const dispatch = useDispatch()
+  const { appActions, searchActions } = useActions()
 
-	// Handlers
-	const handleChange = (e) => {
-		e.preventDefault()
-		setQuery(e.target.value)
-	}
+  // Handlers
+  const handleChange = (e) => {
+    e.preventDefault()
+    setQuery(e.target.value)
+  }
 
-	const handleSubmit = (e) => {
-		e.preventDefault()
-		if (query && query.trim()) {
-			dispatch(searchActions.handleQuery(query))
-		}
-	}
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (query && query.trim()) {
+      dispatch(searchActions.handleQuery(query))
+    }
+  }
 
-	const debouncedQuery = useDebounce(query, 500)
+  const debouncedQuery = useDebounce(query, 500)
 
-	useEffect(() => {
-		if (debouncedQuery) {
-			if (debouncedQuery.length > 2) {
-				dispatch(searchActions.handleQuery(debouncedQuery.trim()))
-			}
-		}
-	}, [debouncedQuery])
+  useEffect(() => {
+    if (debouncedQuery) {
+      if (debouncedQuery.length > 2) {
+        dispatch(searchActions.handleQuery(debouncedQuery.trim()))
+      }
+    }
+  }, [debouncedQuery])
 
-	useEffect(() => {
-		if (query === "") {
-			dispatch(searchActions.clearSearchResults())
-		}
-	}, [query])
+  useEffect(() => {
+    if (query === "") {
+      dispatch(searchActions.clearSearchResults())
+    }
+  }, [query])
 
-	// Output
-	return (
-		<div className="SearchForm">
-			<form onSubmit={handleSubmit}>
-				<div className="form-wrapper">
-					<label>Enter an artist name here</label>
-					<div className="input-group">
-						<input onChange={handleChange} type="text" value={query} />
-						<button className="button">
-							<ArrowRight />
-						</button>
-					</div>
-					<div>
-						<ListSuggestions results={results} />
-					</div>
-				</div>
-			</form>
-		</div>
-	)
+  // Output
+  return (
+    <div className="SearchForm">
+      <form onSubmit={handleSubmit}>
+        <div className="form-wrapper">
+          <label>Enter an artist name here</label>
+          <div className="input-group">
+            <input onChange={handleChange} type="text" value={query} disabled />
+            <button className="button" disabled>
+              <ArrowRight />
+            </button>
+          </div>
+          <small style={{ display: `block`, marginTop: `1rem` }}>
+            Sorry, the Genius API is not playing nicely. But check out the
+            sample data below!
+          </small>
+          <div>
+            <ListSuggestions results={results} />
+          </div>
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default SearchForm
